641017aeeb194ec2eb26249ce0661ac009628a6a
NVV <unitmuc@icloud.com>
2021-09-21 21:34:16 +0300

    end hw 23092021 - aop

--- spring-d2021-09-23-aop/src/main/java/com/nvv/aspects/Security.java
+++ spring-d2021-09-23-aop/src/main/java/com/nvv/aspects/Security.java
@@ -1,5 +1,7 @@
 package com.nvv.aspects;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.core.annotation.Order;
 
@@ -8,4 +10,10 @@ import org.springframework.core.annotation.Order;
 @Order(3)
 public class Security {
  // ToFuture
+//    @Around("execution(public * userDoSomeThing())")
+//    public Object aroundSome(ProceedingJoinPoint proceedingJoinPoint) throws Throwable{
+//        Object targetMethodResult = proceedingJoinPoint.proceed(); // many tips there
+//        // bill vasya stala masha
+//        return targetMethodResult;
+//    }
 }
