677611a5331ed33af3b2d111bbe0c5e1013d11b9
nvv <49232479+uniondirfolder@users.noreply.github.com>
2021-10-11 21:12:40 +0300

    rest
    add new task

---
+++ REST/src/main/java/com/nvv/Config.java
@@ -0,0 +1,51 @@
+package com.nvv;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.hibernate.SessionFactory;
+import org.hibernate.mapping.Property;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackages = "com.nvv")
+@EnableWebMvc
+@EnableTransactionManagement
+public class Config {
+    @Bean
+    public DataSource dataSource() throws PropertyVetoException {
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&amp;serverTimezone=UTC");
+        comboPooledDataSource.setUser("root");
+        comboPooledDataSource.setPassword("rfkbuekf");
+        return comboPooledDataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean localSessionFactoryBean() throws PropertyVetoException {
+        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+        localSessionFactoryBean.setDataSource(dataSource());
+        localSessionFactoryBean.setPackagesToScan("com.nvv");
+        Properties property = new Properties();
+        property.setProperty("hibernate.dialect","org.hibernate.dialect.MySQLDialect");
+        property.setProperty("hibernate.show_sql","true");
+        localSessionFactoryBean.setHibernateProperties(property);
+        return localSessionFactoryBean;
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager() throws PropertyVetoException {
+        HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
+        hibernateTransactionManager.setSessionFactory(localSessionFactoryBean().getObject());
+        return hibernateTransactionManager;
+    }
+}
